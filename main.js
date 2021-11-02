328d4e00bc3a3eec63c538da13e11201404874b3
alireza <alireza.zabihi@yahoo.com>
2021-11-02 19:40:49 +0330

    first edition of the game (console only)

---
+++ main.js
@@ -0,0 +1,85 @@
+let playerScore = 0;
+let computerScore = 0;
+
+//returns a radom choice
+function computerPlay() { 
+    let randomNum = Math.floor(Math.random() * 3)
+    /*
+    if (randomNum === 0) {
+        return "Rock"
+    }
+    else if (randomNum === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }*/
+    return (randomNum === 0) ? "Rock" : (randomNum === 1) ? "Paper" : "Scissors";
+}
+
+//play the game once 
+function play(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection.toLowerCase() === "paper")
+        {
+            return "its a draw!"
+        }
+        else if (computerSelection.toLowerCase() === "rock") {
+            return "You Win! Paper beats Rock"
+        }
+        else {
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection.toLowerCase() === "paper")
+        {
+            return "You Lose! Paper beats Rock"
+        }
+        else if (computerSelection.toLowerCase() === "rock") {
+            return "its a draw!"
+        }
+        else {
+            return "You Win! Rock beats Scissors"
+        }
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection.toLowerCase() === "paper")
+        {
+            return "You Win! Scissors beats Paper"
+        }
+        else if (computerSelection.toLowerCase() === "rock") {
+            return "You Lose! Rock beats Scissors"
+        }
+        else {
+            return "its a draw!"
+        }
+    }   
+}
+
+function game() {
+    //declaring the needed variables
+    let computerSelection;
+    let playerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+    //playing the game for 5 rounds
+    for (let i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
+        playerSelection = prompt("please select your choice (rock, paper, scissors)", "rock");
+        result = play(playerSelection, computerSelection)
+        console.log(result)
+        alert(result)
+        if (result.slice(0, 8) === "You Win!") {
+            playerScore++
+        }
+        else if (result.slice(0, 9) === "You Lose!")
+            computerScore++
+    }
+    result = (computerScore > playerScore) ? "Computer Won!" : (computerScore < playerScore) ? "Congrats You Won :)" : "it was a draw!"
+    console.log(result)
+}
+
+game()
+//console.log(computerPlay())
